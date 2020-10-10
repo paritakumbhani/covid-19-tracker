2ece106e35a297b79f3aa5558de81c9e7ad97b16
Parita Kumbhani <kumb0004@algonquinlive.com>
2020-10-09 23:05:18 -0400

    init -2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import{
+  FormControl,
+  MenuItem,
+ 
+  Select,
+} from "@material-ui/core";
 import './App.css';
 
 function App() {
+  const[countries, setCountries] = useState ([
+    'USA','UK','INDIA'
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app_header">
+      <h1>Covid-19 Tracker</h1>
+      <FormControl className="app_dropdown">
+        <Select 
+        variant = "outlined"
+        var="abc">
+          {/*loop through all the countries and show a drop down list of the countries */}
+
+          {
+            countries.map(country =>(
+              <MenuItem value={country}>{country}</MenuItem>
+            ))
+          }
+         {/* <MenuItem value="worldwide">Worldwide</MenuItem>
+          <MenuItem value="worldwide">Option 2</MenuItem>
+          <MenuItem value="worldwide">Option 3</MenuItem>
+          <MenuItem value="worldwide">Option 4</MenuItem>*/}
+        </Select>
+      </FormControl>
+      </div>
+      
+      {/*Header*/}
+      {/* Title + select input drodown field*/}
+
+      {/*InfoBoxes*/}
+      {/*InfoBoxes*/}
+      {/*InfoBoxes*/}
+
+      {/*Tables */}
+      {/*Graph*/}
+
+      {/*Map*/}
     </div>
   );
 }
 
 export default App;
+ 
