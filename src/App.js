65d07830a37a77584e6a6f6325f7d5cffc3dd2ef
Parita Kumbhani <kumb0004@algonquinlive.com>
2020-10-10 16:44:45 -0400

    Got number by API

--- src/App.js
+++ src/App.js
@@ -1,72 +1,90 @@
 import React, { useEffect, useState } from "react";
-import { FormControl, MenuItem, Select } from "@material-ui/core";
+import { FormControl, MenuItem, Select, Card, CardContent } from "@material-ui/core";
 import InfoBox from './InfoBox';
 import Map from './Map';
 import "./App.css";
 
 function App() {
   const [countries, setCountries] = useState(["USA", "UK", "INDIA"]);
-  const[country, setCountry]= useState("worldwide");
+  const [country, setCountry] = useState("worldwide");
+  const[countryInfo, setCountryInfo] = useState({});
 
-    useEffect(() => {
-      const getCountiresData = async ()=> {
-        await fetch("https://disease.sh/v3/covid-19/countries")
+  useEffect(() => {
+    const getCountiresData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
         .then((response) => response.json())
-        .then((data)=>{
-          const countries = data.map((country)=>(
+        .then((data) => {
+          const countries = data.map((country) => (
             {
-              name:  country.country,
+              name: country.country,
               value: country.countryInfo.iso2
             }
           ));
           setCountries(countries);
         });
-      };
-      getCountiresData();
-    },[]);
+    };
+    getCountiresData();
+  }, []);
 
-    const onCountryChange = async (event) => {
-      const countryCode = event.target.value;
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value;
+    setCountry(countryCode);
+
+    const url = countryCode === 'worldwide' 
+    ? "https://disease.sh/v3/covid-19/all"
+    : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+
+    await fetch(url)
+    .then(response => response.json())
+    .then(data => {
       setCountry(countryCode);
-        };
+      setCountryInfo(data);
+    })
+     
+  };
+  console.log("contry info>>>",countryInfo);
   return (
     <div className="app">
       <div className="app__left">
-      <div className="app_header">
-        <h1>Covid-19 Tracker</h1>
-        <FormControl className="app_dropdown">
-          <Select variant="outlined" onChange={onCountryChange} value={country}>
-            <MenuItem value="worldwide">Worldwide</MenuItem>
-            {countries.map((country) => (
-              <MenuItem value={country.value}>{country.name}</MenuItem>
-            ))}
-          </Select>
-        </FormControl>
+        <div className="app_header">
+          <h1>Covid-19 Tracker</h1>
+          <FormControl className="app_dropdown">
+            <Select variant="outlined" onChange={onCountryChange} value={country}>
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              {countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+
+
+        <div className="app__stats">
+          <InfoBox title="Coronavirus Cases" cases={countryInfo.todayCases} total={countryInfo.cases} />
+
+          <InfoBox title="Recovered" cases={countryInfo.todayRecovered} total={countryInfo.recovered} />
+
+          <InfoBox title="Deaths" cases={countryInfo.todayDeaths} total={countryInfo.deaths} />
+
+        </div>
+
+
+
+
+        {/*Map*/}
+        <Map />
       </div>
 
-    
-     <div className="app__stats">
-       <InfoBox title="Coronavirus Cases"  cases={123} total={2000}/>
+      <Card className="app__right">
+        <CardContent>
+          <h3>Live Cases by Countires</h3>
+          {/*Tables */}
+          <h3>Worldwide new Cases </h3>
+          {/*Graph*/}
+        </CardContent>
+      </Card>
+    </div>
 
-       <InfoBox title="Recovered"  cases={1234} total={3000}/>
-
-       <InfoBox title="Deaths" cases={12345} total={4000}/>
-       
-      </div>         
-      
-
-      
-
-      {/*Map*/}
-      <Map/>
-       </div>
-
-       <div className="app__right">
-         {/*Tables */}
-      {/*Graph*/}
-       </div>
-      </div>
-      
   );
 }
 
