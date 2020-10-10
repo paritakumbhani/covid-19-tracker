2ccd0bf690947e0e780d30ff266e288d25893263
Parita Kumbhani <kumb0004@algonquinlive.com>
2020-10-10 13:25:53 -0400

    List of Countries

--- src/App.js
+++ src/App.js
@@ -1,40 +1,46 @@
-import React, { useState } from 'react';
-import{
-  FormControl,
-  MenuItem,
- 
-  Select,
-} from "@material-ui/core";
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { FormControl, MenuItem, Select } from "@material-ui/core";
+import "./App.css";
 
 function App() {
-  const[countries, setCountries] = useState ([
-    'USA','UK','INDIA'
-  ]);
+  const [countries, setCountries] = useState(["USA", "UK", "INDIA"]);
+  //https://disease.sh/v3/covid-19/countries
+
+    useEffect(() => {
+      const getCountiresData = async ()=> {
+        await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data)=>{
+          const countries = data.map((country)=>(
+            {
+              name:  country.country,
+              value: country.countryInfo.iso2
+            }
+          ));
+          setCountries(countries);
+        });
+      };
+      getCountiresData();
+    },[]);
   return (
     <div className="app">
       <div className="app_header">
-      <h1>Covid-19 Tracker</h1>
-      <FormControl className="app_dropdown">
-        <Select 
-        variant = "outlined"
-        var="abc">
-          {/*loop through all the countries and show a drop down list of the countries */}
+        <h1>Covid-19 Tracker</h1>
+        <FormControl className="app_dropdown">
+          <Select variant="outlined" var="abc">
+            {/*loop through all the countries and show a drop down list of the countries */}
 
-          {
-            countries.map(country =>(
-              <MenuItem value={country}>{country}</MenuItem>
-            ))
-          }
-         {/* <MenuItem value="worldwide">Worldwide</MenuItem>
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+            {/* <MenuItem value="worldwide">Worldwide</MenuItem>
           <MenuItem value="worldwide">Option 2</MenuItem>
           <MenuItem value="worldwide">Option 3</MenuItem>
           <MenuItem value="worldwide">Option 4</MenuItem>*/}
-        </Select>
-      </FormControl>
+          </Select>
+        </FormControl>
       </div>
-      {/* parita */}
-      
+
       {/*Header*/}
       {/* Title + select input drodown field*/}
 
@@ -51,4 +57,3 @@ function App() {
 }
 
 export default App;
- 
