340961a6539b1557bd1e10df572b822bd61a6667
Parita Kumbhani <kumb0004@algonquinlive.com>
2020-10-11 13:09:01 -0400

    sorted data

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import InfoBox from './InfoBox';
 import Map from './Map';
 import Table from './Table';
 import "./App.css";
+import { sortData } from "./util";
 
 function App() {
   const [countries, setCountries] = useState(["USA", "UK", "INDIA"]);
@@ -31,7 +32,8 @@ function App() {
             }
           ));
 
-          setTableData(data);
+          const sortedData = sortData(data);
+          setTableData(sortedData);
           setCountries(countries);
         });
     };
